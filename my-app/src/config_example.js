454efe9fdf90539e8586b7bccce968215957553a
Brianna Tobin <btobin86@gmail.com>
2020-07-26 16:14:07 -0500

    Update config_example.js

--- my-app/src/config_example.js
+++ my-app/src/config_example.js
@@ -3,7 +3,7 @@ export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 // Replace with your app's client ID, redirect URI and desired scopes
 
-export const clientId = "dc73f6974b5142f3b6f40a3f813bc78d";
+export const clientId = "<YOUR CLIENT ID>";
 
 export const redirectUri = "http://localhost:3000/";
 
